824f2c7df464d1a5366e81e3526c5ba23b7c35f8
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-22 00:35:47 +0530

    Create fields for the class product

---
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -0,0 +1,9 @@
+package com.niit.bej.domain;
+
+public class Product {
+    private String productName;
+    private int productId;
+    private int productQuantity;
+    private double productPrice;
+
+}
