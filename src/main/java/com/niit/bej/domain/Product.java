3b27158616c2fb39d8d05714d31e1af713631e3f
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-22 00:36:48 +0530

    Created parameterised and non-argument constructor for the class product

--- src/main/java/com/niit/bej/domain/Product.java
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -6,4 +6,13 @@ public class Product {
     private int productQuantity;
     private double productPrice;
 
+    public Product(String productName, int productId, int productQuantity, double productPrice) {
+        this.productName = productName;
+        this.productId = productId;
+        this.productQuantity = productQuantity;
+        this.productPrice = productPrice;
+    }
+
+    public Product() {
+    }
 }
