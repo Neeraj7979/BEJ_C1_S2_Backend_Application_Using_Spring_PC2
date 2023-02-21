c312119e1e228e49ff3b6072919e7675298b8736
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-22 00:37:35 +0530

    Created getter and setter method for product class

--- src/main/java/com/niit/bej/domain/Product.java
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -15,4 +15,36 @@ public class Product {
 
     public Product() {
     }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
 }
