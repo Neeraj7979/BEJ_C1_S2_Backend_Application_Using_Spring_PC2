9a8775d680fdad359921eba48df933c1d8615af6
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-22 00:47:16 +0530

    Called ApplicationContext class to get bean of product class

--- src/main/java/com/niit/bej/App.java
+++ src/main/java/com/niit/bej/App.java
@@ -1,13 +1,17 @@
 package com.niit.bej;
 
+import com.niit.bej.domain.Product;
+import com.niit.bej.domain.beans.ConfigBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigBean.class);
+        Product product1 = applicationContext.getBean(Product.class);
+        System.out.println(product1.getProductName());
     }
 }
--- src/main/java/com/niit/bej/domain/beans/ConfigBean.java
+++ src/main/java/com/niit/bej/domain/beans/ConfigBean.java
@@ -1,6 +1,11 @@
 package com.niit.bej.domain.beans;
 
+import com.niit.bej.domain.Product;
+import org.springframework.context.annotation.Bean;
+
 public class ConfigBean {
-
-
+    @Bean
+    public Product getProductDetails() {
+        return new Product("Samsung-Galaxy", 101, 100, 12200);
+    }
 }
